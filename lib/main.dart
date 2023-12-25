bc47fdfba8437d2d47de7b8268679481ffa951a8
masundareza123 <ngekngek328@gmail.com>
2023-12-25 23:36:36 +0700

    update lumayan

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('Netflix'), centerTitle: true),
-        body: Text('Hello'),
-      ),
+          appBar: AppBar(
+            title: RichText(
+              text: const TextSpan(
+                  text: 'Hi, ',
+                  style: TextStyle(fontSize: 18),
+                  children: [
+                    TextSpan(
+                        text: 'M. Aji Perdana',
+                        style: TextStyle(fontWeight: FontWeight.bold))
+                  ]),
+            ),
+            backgroundColor: Colors.red,
+          ),
+          body: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Hello Flutter',
+            ),
+          )),
     );
   }
 }
